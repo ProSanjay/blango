c64fc5f3942f40392657f8db54bd1f43b0b17396
Sanjay kushwah <058sanjaykushwah@gmail.com>
2024-10-05 18:58:13 +0000

    Finish Introduction and Django Admin

--- blango/settings.py
+++ blango/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
